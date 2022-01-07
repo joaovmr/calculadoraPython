45a211ef5d4e2fd395db81e6fc384f629d2cd652
joaovmr <48189739+joaovmr@users.noreply.github.com>
2022-01-07 18:42:09 -0300

    adding mores tests

--- calc.py
+++ calc.py
@@ -1,21 +1,43 @@
 def add(a, b):
     return a + b
 
-def test_add():
-    assert add(2, 3) == 5
-
 def subtract(a, b):
     return a - b 
 
-def test_subtract():
-   assert subtract(2, 3) == -1
+def multiplication(a, b):
+    return a * b
 
-def multiplication():
-   return a * b
+def division(a, b):
+    return division(4,2) == 2
+
+def equal(a,b):
+    return a == b
+
+def test_add():
+    assert add(2, 3) == 5
+
+def test_subtract():
+    assert subtract(2, 3) == -1
 
 def test_multiplication():
-   assert multiplication(2, 3) == 6
+    assert multiplication(2, 3) == 6
 
-def division():
-   return division(4,2) == 2
+def test_division():
+    assert division(4, 2) == 2
+
+def test_equal():
+    assert equal(2, 2) == True
+
+def test_notEqual():
+    assert equal(2, 3) == False
+
+def test_multiplicationZero():
+    assert multiplication(100, 0) == 0
+
+def test_divisionZero():
+    assert division(0, 100) == 0
+
+def test_subtractAdd():
+    assert (subtract(3,4) + add(3,4)) == 6
+
 
