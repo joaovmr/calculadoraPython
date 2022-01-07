05817c7fc608099186646ed29c75a77cd7426562
joaovmr <48189739+joaovmr@users.noreply.github.com>
2022-01-07 18:48:07 -0300

    correcting the last test

--- calc.py
+++ calc.py
@@ -40,7 +40,7 @@ def test_divisionZero():
 def test_subtractAdd():
     assert (subtract(3,4) + add(3,4)) == 6
 
-def test_subtractAdd():
-    assert (equal((subtract(3,4) + add(3,4),6))) == True
+def test_equalOnOthers():
+    assert equal(multiplication(2,4),8) == True
 
 
