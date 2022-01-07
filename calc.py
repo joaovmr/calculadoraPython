08ace45b34ee70fe5fb5a7d28d4d2efc6299fa7d
joaovmr <48189739+joaovmr@users.noreply.github.com>
2022-01-07 18:35:00 -0300

    correction on subtract. Adding division and multiplication

--- calc.py
+++ calc.py
@@ -1,15 +1,21 @@
 def add(a, b):
     return a + b
 
-
 def test_add():
     assert add(2, 3) == 5
-    assert add('space', 'ship') == 'spaceship'
-
 
 def subtract(a, b):
-    return a + b  # <--- fix this in step 7
-
+    return a - b 
 
 def test_subtract():
    assert subtract(2, 3) == -1
+
+def multiplication():
+   return a * b
+
+def test_multiplication():
+   assert multiplication(2, 3) == 6
+
+def division():
+   return division(4,2) == 2
+
